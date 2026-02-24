b99fd98e9768e506d568d51a1a2283291a3dbdd7
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-24 09:06:11 +0500

    Add multi logging adapter

---
+++ internal/adapters/logging/mult.go
@@ -0,0 +1,39 @@
+package logging
+
+import (
+	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/core/ports"
+)
+
+type multi struct {
+	loggers []ports.Logger
+}
+
+func NewMultiLogger(loggers ...ports.Logger) ports.Logger {
+	return &multi{
+		loggers: loggers,
+	}
+}
+
+func (m *multi) Debug(msg string, args ...any) {
+	for _, logger := range m.loggers {
+		logger.Debug(msg, args...)
+	}
+}
+
+func (m *multi) Info(msg string, args ...any) {
+	for _, logger := range m.loggers {
+		logger.Info(msg, args...)
+	}
+}
+
+func (m *multi) Warn(msg string, args ...any) {
+	for _, logger := range m.loggers {
+		logger.Debug(msg, args...)
+	}
+}
+
+func (m *multi) Error(msg string, args ...any) {
+	for _, logger := range m.loggers {
+		logger.Error(msg, args...)
+	}
+}
