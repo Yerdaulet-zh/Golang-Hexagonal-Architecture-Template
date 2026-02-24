aaab0e2f806f20b518d7eae7b395f4c04e43692c
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-24 16:44:53 +0500

    Optimize struct and disable govet linting

--- internal/adapters/config/kafka.go
+++ internal/adapters/config/kafka.go
@@ -6,24 +6,25 @@ import (
 	"github.com/spf13/viper"
 )
 
+//nolint:govet
 type KafkaConfig struct {
 	Brokers []string
 	Topic   string
+
+	// Compression: "snappy", "gzip", "lz4", or "zstd"
+	Compression string
+
+	// Performance / Batching settings
+	BatchBytes      int64         // Default: 1MB
+	BatchSize       int           // Default: 100
+	BatchTimeout    time.Duration // Default: 1s
+	WriteBackoffMin time.Duration // Default: 100ms
 
 	// Reliability settings
 	MaxAttempts  int           // Default: 10
 	RequiredAcks int           // -1 = All replicas
 	WriteTimeout time.Duration // Default: 10s
 	ReadTimeout  time.Duration // Default: 10s
-
-	// Performance / Batching settings
-	BatchSize       int           // Default: 100
-	BatchBytes      int64         // Default: 1MB
-	BatchTimeout    time.Duration // Default: 1s
-	WriteBackoffMin time.Duration // Default: 100ms
-
-	// Compression: "snappy", "gzip", "lz4", or "zstd"
-	Compression string
 }
 
 func NewKafkaConfig() *KafkaConfig {
