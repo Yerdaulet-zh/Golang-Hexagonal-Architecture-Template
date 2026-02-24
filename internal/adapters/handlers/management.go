5bdc5394b9df0246569300ca9bb22c27f26b1b0d
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-24 09:08:02 +0500

    Add readiness check route

--- internal/adapters/handlers/management.go
+++ internal/adapters/handlers/management.go
@@ -4,14 +4,18 @@ package handlers
 
 import (
 	"net/http"
+
+	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/core/ports"
 )
 
 // HealthHandler defines the dependencies for health checks
-type HealthHandler struct{}
+type HealthHandler struct {
+	dbClient ports.Database
+}
 
 // NewHealthHandler creates a new instance of HealthHandler.
-func NewHealthHandler() *HealthHandler {
-	return &HealthHandler{}
+func NewHealthHandler(dbClient ports.Database) *HealthHandler {
+	return &HealthHandler{dbClient: dbClient}
 }
 
 // Healthz handles the health check requests.
@@ -19,4 +23,17 @@ func (h *HealthHandler) Healthz(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	//nolint:errcheck,gosec // No need to check errors when writing simple health status
 	w.Write([]byte("OK"))
+}
+
+// Ready handles the accessibility of PostgreSQL database
+func (h *HealthHandler) Ready(w http.ResponseWriter, r *http.Request) {
+	if err := h.dbClient.Ping(r.Context()); err != nil {
+		w.WriteHeader(http.StatusServiceUnavailable)
+		//nolint:errcheck,gosec
+		w.Write([]byte("Service Unavailable: Database unreachable"))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	//nolint:errcheck,gosec
+	w.Write([]byte("Ready"))
 }
