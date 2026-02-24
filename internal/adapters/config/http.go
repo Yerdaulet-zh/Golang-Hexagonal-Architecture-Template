7cfd9efc255fc15f33a6a3aaecb5801008a66dfd
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-24 09:41:24 +0500

    Make parameters CamelCase

--- internal/adapters/config/http.go
+++ internal/adapters/config/http.go
@@ -3,20 +3,20 @@ package config
 import "github.com/spf13/viper"
 
 type httpConfig struct {
-	http_management_addr string
-	http_business_addr   string
+	httpManagementAddr string
+	httpBusinessAddr   string
 }
 
 func NewHttpConfig() *httpConfig {
 	return &httpConfig{
-		http_management_addr: viper.GetString("http.management_addr"),
-		http_business_addr:   viper.GetString("http.business_addr"),
+		httpManagementAddr: viper.GetString("http.management_addr"),
+		httpBusinessAddr:   viper.GetString("http.business_addr"),
 	}
 }
 
 func (c *httpConfig) HttpManagementAddr() string {
-	return c.http_management_addr
+	return c.httpManagementAddr
 }
 func (c *httpConfig) HttpBusinessAddr() string {
-	return c.http_business_addr
+	return c.httpBusinessAddr
 }
