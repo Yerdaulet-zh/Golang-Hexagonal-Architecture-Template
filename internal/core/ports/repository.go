f26219f8cc7b0004c2e071a6aed1841e3b6e3fa9
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-24 09:44:10 +0500

    Add comment

--- internal/core/ports/repository.go
+++ internal/core/ports/repository.go
@@ -2,6 +2,9 @@ package ports
 
 import "context"
 
+// Database defines the set of methods required for infrastructure
+// health checks and connection lifecycle management.
+// This interface is used for mocking the postgre.Client
 type Database interface {
 	Ping(ctx context.Context) error
 	Close() error
