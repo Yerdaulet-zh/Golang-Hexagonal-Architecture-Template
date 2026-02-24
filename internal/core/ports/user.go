9ac23cc6d0a4e690ee025a1657e1a5c7c31f268f
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-24 09:44:29 +0500

    Add package comment

--- internal/core/ports/user.go
+++ internal/core/ports/user.go
@@ -1,3 +1,7 @@
+// Package ports defines the interfaces that bridge the core logic with external adapters.
 package ports
 
-type User interface{}
+type User interface {
+	// Create(ctx context.Context, u *domain.User) error
+	// GetByID(ctx context.Context, id string) (*domain.User)
+}
