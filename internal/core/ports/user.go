76be19bd67b46ecedf9c84135e5aed72d892b26e
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-25 14:34:56 +0500

    Define Notification interface and its Notify Method

--- internal/core/ports/user.go
+++ internal/core/ports/user.go
@@ -1,7 +1,8 @@
 // Package ports defines the interfaces that bridge the core logic with external adapters.
 package ports
 
-type User interface {
-	// Create(ctx context.Context, u *domain.User) error
-	// GetByID(ctx context.Context, id string) (*domain.User)
+import "context"
+
+type Notification interface {
+	Notify(ctx context.Context, email string) error
 }
