f049349f98f3fedeedcea7b76871c1cea1841bd1
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-25 16:59:10 +0500

    Rename fucntion name

--- internal/core/domain/notification/utils.go
+++ internal/core/domain/notification/utils.go
@@ -6,7 +6,7 @@ import (
 	"github.com/badoux/checkmail"
 )
 
-func IsValidHost(email string) error {
+func ValidateEmailHost(email string) error {
 	if err := checkmail.ValidateHost(email); err != nil {
 		return fmt.Errorf("Email service validation error: %s", err.Error())
 	}
