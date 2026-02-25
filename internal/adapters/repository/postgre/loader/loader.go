5dd4698d7b5958eb18db01e52d7141fad8b11d0b
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-25 14:29:21 +0500

    Add atlas postgres sql schema generator logic for migration

---
+++ internal/adapters/repository/postgre/loader/loader.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"ariga.io/atlas-provider-gorm/gormschema"
+	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/adapters/repository/postgre/persistency/notification"
+
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+func main() {
+	config := &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	}
+	stmts, err := gormschema.New("postgres", gormschema.WithConfig(config)).Load(
+		&notification.EmailNotification{},
+	)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to load gorm schema: %v\n", err)
+		os.Exit(1)
+	}
+	io.WriteString(os.Stdout, stmts)
+}
