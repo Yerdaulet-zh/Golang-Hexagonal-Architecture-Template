e9285f190b7546f617218d92f5aa4aeb86584f0e
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-26 11:36:49 +0500

    Handle Database error

--- internal/adapters/repository/postgre/notification_repo.go
+++ internal/adapters/repository/postgre/notification_repo.go
@@ -4,16 +4,17 @@ import (
 	"context"
 
 	persistency "gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/adapters/repository/postgre/persistency/notification"
+	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/core/domain"
 	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/core/ports"
 	"gorm.io/gorm"
 )
 
 type NotificationRepo struct {
 	db     *gorm.DB
-	logger *ports.Logger
+	logger ports.Logger
 }
 
-func NewNotificationRepository(db *gorm.DB, logger *ports.Logger) *NotificationRepo {
+func NewNotificationRepository(db *gorm.DB, logger ports.Logger) *NotificationRepo {
 	return &NotificationRepo{
 		db:     db,
 		logger: logger,
@@ -21,8 +22,12 @@ func NewNotificationRepository(db *gorm.DB, logger *ports.Logger) *NotificationR
 }
 
 func (r *NotificationRepo) Email(ctx context.Context, email string, message string) error {
-	return gorm.G[persistency.EmailNotification](r.db).Create(ctx, &persistency.EmailNotification{
+	if err := gorm.G[persistency.EmailNotification](r.db).Create(ctx, &persistency.EmailNotification{
 		Email:   email,
 		Message: message,
-	})
+	}); err != nil {
+		r.logger.Error(domain.LogLevelRepository, "Error while creating a record", err)
+		return domain.ErrDatabaseInternalError
+	}
+	return nil
 }
