299f046f903e2068e1e68cd8d7663ecca86b54b2
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-26 10:04:14 +0500

    Add sanitizer of client input data

--- internal/adapters/handlers/dto/notification_request.go
+++ internal/adapters/handlers/dto/notification_request.go
@@ -1,6 +1,13 @@
 package dto
 
+import "strings"
+
 type EmailNotification struct {
 	Email   string `json:"email" validate:"required,email"`
 	Message string `json:"message" validate:"required,max=255"`
+}
+
+func (r *EmailNotification) Sanitize() {
+	r.Email = strings.TrimSpace(r.Email)
+	r.Message = strings.TrimSpace(r.Message)
 }
