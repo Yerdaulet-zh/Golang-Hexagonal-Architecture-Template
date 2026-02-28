9fc8e7aa8e97cc6ee223140720464566e6c0c995
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-28 09:46:03 +0500

    Enable middleware exectuions at musiness logic, use IP Rate Limiter

--- cmd/servers/router.go
+++ cmd/servers/router.go
@@ -3,11 +3,24 @@ package servers
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/adapters/cache/redis"
 	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/adapters/handlers"
+	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/adapters/handlers/middleware"
 	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/core/ports"
 )
+
+// Defining middleware
+type mw func(http.Handler) http.Handler
+
+func applyMiddlewares(h http.Handler, mws ...mw) http.Handler {
+	for _, mw := range mws {
+		h = mw(h)
+	}
+	return h
+}
 
 // MapManagementRoutes maps management-related routes (health checks, metrics) to their handlers.
 func MapManagementRoutes(logger ports.Logger, client ports.Database) http.Handler {
@@ -21,10 +34,16 @@ func MapManagementRoutes(logger ports.Logger, client ports.Database) http.Handle
 	return mux
 }
 
-func MapBusinessRoutes(logger ports.Logger, NotificationService ports.NotificationUseCase) http.Handler {
+func MapBusinessRoutes(logger ports.Logger, rdb ports.Redis, NotificationService ports.NotificationUseCase) http.Handler {
 	mux := http.NewServeMux()
 
 	notification := handlers.NewNotificationHandler(NotificationService, logger)
 	mux.HandleFunc("POST /v1/notification/email", notification.EmailNotification)
-	return mux
+
+	// Middlewares
+	rateLimiter := redis.NewRateLimiter(logger, rdb)
+	middlewares := []mw{
+		middleware.NewIPRateLimiter(rateLimiter, 100*time.Second, 1),
+	}
+	return applyMiddlewares(mux, middlewares...)
 }
