0f6e58149729103f0395ebced3e5d736e83691cc
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-28 09:44:04 +0500

    Redis client now uses DI with the interface and implements interface methods

--- internal/adapters/cache/redis/client.go
+++ internal/adapters/cache/redis/client.go
@@ -1,17 +1,56 @@
-// Package redis provides the implementation for caching and data storage
-// using the Redis key-value store. It acts as a driven adapter in the
-// hexagonal architecture, satisfying infrastructure ports defined by the core.
+// // Package redis provides the implementation for caching and data storage
+// // using the Redis key-value store. It acts as a driven adapter in the
+// // hexagonal architecture, satisfying infrastructure ports defined by the core.
 package redis
 
 import (
+	"context"
+
 	"github.com/redis/go-redis/v9"
 	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/adapters/config"
+	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/core/ports"
 )
 
-func NewRedisClient(cfg *config.RedisConfig) *redis.Client {
-	return redis.NewClient(&redis.Options{
+type redisAdapter struct {
+	client *redis.Client
+}
+
+func NewRedisClient(cfg *config.RedisConfig) ports.Redis {
+	client := redis.NewClient(&redis.Options{
 		Addr:     cfg.Addr(),
 		Password: cfg.RedisPassword,
 		DB:       cfg.DB,
 	})
+	return &redisAdapter{client: client}
+}
+
+func (a *redisAdapter) Pipeline() redis.Pipeliner {
+	return a.client.Pipeline()
+}
+
+func (a *redisAdapter) Close() error {
+	return a.client.Close()
+}
+
+func (a *redisAdapter) Eval(ctx context.Context, script string, keys []string, args ...interface{}) *redis.Cmd {
+	return a.client.Eval(ctx, script, keys, args...)
+}
+
+func (a *redisAdapter) EvalSha(ctx context.Context, sha1 string, keys []string, args ...interface{}) *redis.Cmd {
+	return a.client.EvalSha(ctx, sha1, keys, args...)
+}
+
+func (a *redisAdapter) EvalRO(ctx context.Context, script string, keys []string, args ...interface{}) *redis.Cmd {
+	return a.client.EvalRO(ctx, script, keys, args...)
+}
+func (a *redisAdapter) EvalShaRO(ctx context.Context, sha1 string, keys []string, args ...interface{}) *redis.Cmd {
+	return a.client.EvalShaRO(ctx, sha1, keys, args...)
+}
+
+func (a *redisAdapter) ScriptExists(ctx context.Context, hashes ...string) *redis.BoolSliceCmd {
+	return a.client.ScriptExists(ctx, hashes...)
+}
+
+func (a *redisAdapter) ScriptLoad(ctx context.Context, script string) *redis.StringCmd {
+	return a.client.ScriptLoad(ctx, script)
 }
