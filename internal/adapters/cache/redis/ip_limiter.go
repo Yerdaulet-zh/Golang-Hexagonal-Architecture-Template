0ae85b9c856e0be5d18fbaa115caaba40a58f672
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-28 09:45:00 +0500

    Add Redis Rate Limiter with lua script

---
+++ internal/adapters/cache/redis/ip_limiter.go
@@ -0,0 +1,57 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/core/domain"
+	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/core/ports"
+)
+
+type redisRateLimiter struct {
+	logger ports.Logger
+	client ports.Redis
+}
+
+func NewRateLimiter(logger ports.Logger, client ports.Redis) ports.RateLimiter {
+	return &redisRateLimiter{
+		logger: logger,
+		client: client,
+	}
+}
+
+var rateLimitScript *redis.Script = redis.NewScript(`
+	redis.call('ZREMRANGEBYSCORE', KEYS[1], 0, ARGV[1])
+	local current_count = redis.call('ZCARD', KEYS[1])
+	if current_count < tonumber(ARGV[4]) then
+		redis.call('ZADD', KEYS[1], ARGV[2], ARGV[5])
+		redis.call('EXPIRE', KEYS[1], ARGV[3])
+		return 1
+	end
+	return 0
+`)
+
+func (r *redisRateLimiter) Allow(ctx context.Context, key string, window time.Duration, limit int64) (bool, error) {
+	key = "ip_rate_limit:" + key
+
+	now := time.Now().UnixMilli()
+	windowStart := now - window.Milliseconds()
+	member := fmt.Sprintf("%d", windowStart)
+
+	result, err := rateLimitScript.Run(ctx, r.client, []string{key},
+		windowStart,           // ARGV[1]
+		now,                   // ARGV[2]
+		int(window.Seconds()), // ARGV[3]
+		limit,                 // ARGV[4]
+		member,                // ARGV[5]
+	).Int()
+	if err != nil {
+		r.logger.Error(domain.LogLevelMiddleware, "Redis IP limiter error:", err)
+		return true, err
+	}
+
+	// result: 1 allow, 0 deny
+	return result == 1, nil
+}
