bfb68375fe87e5529d7e0060907591207bf346e0
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-03-01 14:07:48 +0500

    Adjust loki send method to take care of span and trace IDs from provided context

--- internal/adapters/logging/loki.go
+++ internal/adapters/logging/loki.go
@@ -2,12 +2,14 @@ package logging
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 
 	"gitlab.com/yerdaulet.zhumabay/golang-hexagonal-architecture-template/internal/core/ports"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type lokiAdapter struct {
@@ -22,8 +24,23 @@ func NewLokiLogger(url string, labels map[string]string) ports.Logger {
 	}
 }
 
-func (l *lokiAdapter) send(level, msg string, args ...any) {
+func (l *lokiAdapter) send(ctx context.Context, level, msg string, args ...any) {
+	// Extract TraceID and SpanID from context
+	spanContext := trace.SpanFromContext(ctx).SpanContext()
+
+	var traceID, spanID string
+	if spanContext.HasTraceID() {
+		traceID = spanContext.TraceID().String()
+	}
+	if spanContext.HasSpanID() {
+		spanID = spanContext.SpanID().String()
+	}
+
+	// Start the log line with Trace/Span IDs if they exist
 	line := fmt.Sprintf("level=%s msg=%q", level, msg)
+	if traceID != "" {
+		line = fmt.Sprintf("%s trace_id=%s span_id=%s", line, traceID, spanID)
+	}
 
 	// Loop through args to create individual rows/keys
 	// We assume args come in pairs: ["key", value, "key2", value2]
@@ -72,18 +89,18 @@ func (l *lokiAdapter) send(level, msg string, args ...any) {
 	}
 }
 
-func (l *lokiAdapter) Debug(msg string, args ...any) {
-	l.send("Debug", msg, args...)
+func (l *lokiAdapter) Debug(ctx context.Context, msg string, args ...any) {
+	l.send(ctx, "Debug", msg, args...)
 }
 
-func (l *lokiAdapter) Info(msg string, args ...any) {
-	l.send("Info", msg, args...)
+func (l *lokiAdapter) Info(ctx context.Context, msg string, args ...any) {
+	l.send(ctx, "Info", msg, args...)
 }
 
-func (l *lokiAdapter) Warn(msg string, args ...any) {
-	l.send("Warn", msg, args...)
+func (l *lokiAdapter) Warn(ctx context.Context, msg string, args ...any) {
+	l.send(ctx, "Warn", msg, args...)
 }
 
-func (l *lokiAdapter) Error(msg string, args ...any) {
-	l.send("Error", msg, args...)
+func (l *lokiAdapter) Error(ctx context.Context, msg string, args ...any) {
+	l.send(ctx, "Error", msg, args...)
 }
